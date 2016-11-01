1398fe58b13b0165264e5c16e0a18c54835eb8e2
Minions1128 <zhejian_shen@163.com>
2016-11-01 15:17:56 +0800

    Update 8_assert.py

--- 8_assert.py
+++ 8_assert.py
@@ -3,7 +3,14 @@
 # Oct 27th, 2016
 
 """ assert:
-    可以使用python -O 来忽略assert"""
+    可以使用python -O 来忽略assert
+    使用assert的一些规则：
+    1. 不要滥用
+    2. 要先使用Python自身的异常处理
+    3. 不要使用assert来判断用户输入的内容
+    4. 函数调用后，返回值可以使用assert
+    5. 如果后续业务逻辑继续下去的先决条件可以使用assert"""
+
 x = 1
 y = 2
 assert x == y, 'NOT EQUAL'
